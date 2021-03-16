8fe9c44d5c3b4748df4596167deb2dcc3933470f
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-16 13:52:14 +0530

    Simply getting the data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello From App Component</h1>
+    <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search Currency</h1>
+
+        <form>
+          <input type="text" className="coin-input" placeholder="Search" />
+        </form>
+      </div>
     </div>
   );
 }
