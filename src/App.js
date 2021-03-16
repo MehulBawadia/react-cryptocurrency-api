b37e02cd1a6e489ba839ad8800a5b3c8da4a3637
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-16 14:07:32 +0530

    Added coin component skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     axios
@@ -18,15 +20,43 @@ function App() {
       });
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Search Currency</h1>
 
         <form>
-          <input type="text" className="coin-input" placeholder="Search" />
+          <input
+            type="text"
+            className="coin-input"
+            placeholder="Search"
+            onChange={handleChange}
+          />
         </form>
       </div>
+
+      {filteredCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
